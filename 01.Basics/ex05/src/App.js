63f0f74658fe04319ba1b4094c5a98d42b892b1a
jelypvid <jyspark46@gmail.com>
2024-07-10 15:01:31 +0900

    react basics #05

--- 01.Basics/ex05/src/App.js
+++ 01.Basics/ex05/src/App.js
@@ -1,6 +1,10 @@
+import React from 'react';
+
 function App() {
-    const App = document.createElement('div');
-    App.textContent = "Hello World";
+    // const App = document.createElement('div');
+    // App.textContent = "Hello World";
+    
+    const App = React.createElement('div', null, React.createElement('p', null, 'Hello World'));
     
     return App;
 }
