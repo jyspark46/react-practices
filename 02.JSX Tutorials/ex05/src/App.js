61f2eb24084fd06fa50699db96b351e5b6b1ca27
jelypvid <jyspark46@gmail.com>
2024-07-11 13:35:12 +0900

    jsx tutorials #05

--- 02.JSX Tutorials/ex05/src/App.js
+++ 02.JSX Tutorials/ex05/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Header from './Header';
 import Contents from './Contents';
 
-function App() {
-    return (
-        <div>
+class App extends Component {
+    render() {
+        return (
+          <div>
             <Header />
             <Contents />
-        </div>
-    );
+          </div>
+        );
+    }
 }
 
-export {App};
+export default App;
