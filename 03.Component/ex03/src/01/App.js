4f1c7cf8cb32122f23f3ab8e5092baca8132fb0d
jelypvid <jyspark46@gmail.com>
2024-07-12 15:40:50 +0900

    event #01

--- 03.Component/ex03/src/01/App.js
+++ 03.Component/ex03/src/01/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
     return (
         <div id={'App'}>
-            <h1>
+            <h1 onClick={function() {
+                console.log('click!');
+            }}>
                 Inline Handler(Click Here!)
             </h1>
         </div>
--- 03.Component/ex03/src/03/App.js
+++ 03.Component/ex03/src/03/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default function App() {
 
-
     return (
         <>
             <h2>ex03 - SyntheticEvent</h2>
@@ -13,11 +12,18 @@ export default function App() {
             <form
                 name='addForm'
                 method='post'
-                action='/do/not/go'>
+                action='/do/not/go'
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    console.log(event.target.name + ' : ' + 'AJAX로 등록할 겁니다.');
+                }}>
                 <input
                     type='text'
                     name='message'
-                    placeholder='메세지를 입력 하세요' />
+                    placeholder='메세지를 입력 하세요'
+                    onChange={(Event) => {
+                        console.log(event.target.names  + " : " + event.target.value);
+                    }} />
                 <br/>
                 <br/>
                 <input
