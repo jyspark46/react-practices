474a49df1b9a5a6a1dc0cb573e97b74c6a2d24e8
jelypvid <jyspark46@gmail.com>
2024-07-12 15:05:42 +0900

    tabbox update

--- tabbox/frontend/config/webpack.config.js
+++ tabbox/frontend/config/webpack.config.js
@@ -2,35 +2,46 @@ const path = require('path');
 
 module.exports = function(env) {
     return {
-        mode: "none",
-        entry: path.resolve(`src/index.js`),
-        output: {
-            path: path.resolve('public'),
-            filename: 'assets/js/main.js',
-            assetModuleFilename: 'assets/images/[hash][ext]'
-        },
-        module: {
-            rules:[{
-                test: /\.js/i,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
+      mode: "none",
+      entry: path.resolve(`src/index.js`),
+      output: {
+        path: path.resolve("public"),
+        filename: "assets/js/main.js",
+        assetModuleFilename: "assets/images/[hash][ext]",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js/i,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+              configFile: path.resolve("config/babel.config.json"),
+            },
+          }, {
+            test: /\.(c|sa|sc)ss$/i,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
                 options: {
-                    configFile: path.resolve('config/babel.config.json')
-                }
-            }, {
-                test: /\.(c|sa|sc)ss$/i,
-                use:['style-loader', 'css-loader', 'sass-loader']
-            }, {
-                test: /\.(png|gif|jp?eg|svg|ico|tif?f|bmp)/i,
-                type: 'asset/resource'
-            }]
-        },
-        devServer: {
-            host: '0.0.0.0',
-            port: 9090,
-            liveReload: true,
-            compress: true,
-            hot: false
-        }    
+                  modules: true,
+                },
+              },
+              "sass-loader",
+            ],
+          }, {
+            test: /\.(png|gif|jp?eg|svg|ico|tif?f|bmp)/i,
+            type: "asset/resource",
+          },
+        ],
+      },
+      devServer: {
+        host: "0.0.0.0",
+        port: 9090,
+        liveReload: true,
+        compress: true,
+        hot: false,
+      },
     };
 }
