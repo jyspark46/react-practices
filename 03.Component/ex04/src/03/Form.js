854004a0bc59a4b2bd67c797471f9a7d1912b1c0
jelypvid <jyspark46@gmail.com>
2024-07-15 15:57:04 +0900

    state #03

--- 03.Component/ex04/src/03/Form.js
+++ 03.Component/ex04/src/03/Form.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './assets/Form.css';
 
 export default function Form() {
+    
     return (
         <form
             id="loginForm"
             name="loginForm"
             method="post"
-            action="/do/not/post">
+            action="/do/not/post"
+            onSubmit={e => {
+                e.preventDefault();
+
+                console.log(e.target.email.value, e.target.password.value);
+                console.log("validation codes...");
+                console.log("ajax 로그인...");
+            }}>
 
             <label htmlFor="email">이메일</label>
             <input id="email" name="email" type="text" autoComplete="off" />
