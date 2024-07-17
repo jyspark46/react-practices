8bf6e792be03e2271caeb50ce6f7d08b11ff5b1f
jelypvid <jyspark46@gmail.com>
2024-07-17 11:08:48 +0900

    init commit

--- kanbanboard/frontend/src/KanbanBoard.js
+++ kanbanboard/frontend/src/KanbanBoard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import boards from './assets/json/data.js';
 
 function KanbanBoard() {
     return (
-        <>  
-        </>
+        <div className={''}> 
+            <CardList cards={data.filter(e => e.status ==='ToDo')} Title={'ToDo'} />
+            <CardList cards={data.filter(e => e.status ==='Doing')} Title={'Doing'} />
+            <CardList cards={data.filter(e => e.status ==='Done')} Title={'Done'} />
+        </div>
     );
 }
 
