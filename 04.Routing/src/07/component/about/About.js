4112587982d3554d437b25c5cffd054996678c0b
jelypvid <jyspark46@gmail.com>
2024-07-19 13:06:30 +0900

    route #finish

--- 04.Routing/src/07/component/about/About.js
+++ 04.Routing/src/07/component/about/About.js
@@ -7,7 +7,7 @@ export default function About() {
     return (
         <SiteLayout>
             <div className={styles.About}>
-                <h2>kickscar 입니다.</h2>
+                <h2>JelYPvid 입니다.</h2>
             </div>
         </SiteLayout>
     );
--- 04.Routing/src/08/component/about/About.js
+++ 04.Routing/src/08/component/about/About.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import * as styles from '../../assets/scss/component/About.scss';
 
 export default function About() {
+    const navigate = useNavigate();
+
+    setTimeout(() => {
+        // window.location.href='/error'; 함부로 쓰면 안됨 !!
+        navigate('/error');
+    }, 2000);
 
     return (
             <div className={styles.About}>
-                <h2>kickscar 입니다.</h2>
+                <h2>JelYPvid 입니다.</h2>
             </div>
     );
 }
